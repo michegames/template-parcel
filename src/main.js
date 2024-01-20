d960c46181be0c506db87a5238e0c3101155230f
Michelangelo Giacomelli <michelangelog@gmail.com>
2024-01-20 15:21:41 +0100

    making orientation resume pause

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import { GameOver } from "./scenes/GameOver";
 import { MainMenu } from "./scenes/MainMenu";
 import { Preloader } from "./scenes/Preloader";
 
+function init_game(config, width) {
+  let tmp = Object.assign({}, config);
+  tmp.width = width;
+  const game = new Phaser.Game(tmp);
+  game.config.info = {
+    area: {
+      w: 1031,
+      h: 580,
+    },
+    res: {
+      w: width,
+      h: 580,
+    },
+  };
+  return game;
+}
+
 const ratio = window.innerWidth / innerHeight;
 const height = 580;
 const width = Math.min(1300, Math.round(580 * ratio));
@@ -21,17 +38,35 @@ const config = {
   scene: [Boot, Preloader, MainMenu, Game, GameOver],
 };
 
-const game = new Phaser.Game(config);
-window.$G = game;
-game.config.info = {
-  area: {
-    w: 1031,
-    h: 580,
+let game = null;
+
+if (window.innerWidth > innerHeight) {
+  game = init_game(config, width);
+}
+
+let last_scene = null;
+
+window.addEventListener(
+  "resize",
+  function () {
+    if (window.innerWidth > innerHeight) {
+      if (game === null) {
+        const ratio = window.innerWidth / innerHeight;
+        const width = Math.min(1300, Math.round(580 * ratio));
+        game = init_game(config, width);
+      } else {
+        game.scene.resume(last_scene);
+      }
+    } else {
+      if (game === null) return;
+      let scene = game.scene.getScenes(true);
+      if ("length" in scene && scene.length > 0 && "scene" in scene[0]) {
+        last_scene = scene[0].scene.key;
+        game.scene.pause(last_scene);
+      }
+    }
   },
-  res: {
-    w: width,
-    h: 580,
-  },
-};
+  false,
+);
 
 export default game;
